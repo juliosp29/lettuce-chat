46f7f3d45c186497987c2e88d0e40cd5b61e179d
juliosp29 <85320759+juliosp29@users.noreply.github.com>
2026-04-18 19:28:44 -0400

    Adds client unicast messaging feature

--- feature2/client.py
+++ feature2/client.py
@@ -1,17 +1,55 @@
-# Algorithm 4: High-Level Client Logic — One-to-One Chat (Feature 2)
-#
-# Create a TCP client socket
-# Connect to the server using the provided IP address and port
-# Wait for the server to request a username
-# Input the username from the user and send it to the server
-#
-# Start a background thread to continuously:
-#     Receive incoming messages from the server
-#     Display the received messages to the user
-#
-# In the main thread, repeatedly:
-#     Accept user input from the keyboard
-#     To send a private message, type in the format @username message
-#     Send the message to the server for delivery to the target user
-#
-# If the server disconnects or an error occurs, close the socket connection
+import argparse
+import socket
+import threading
+
+def handle_server(client_socket: socket.socket) -> None:
+    try:
+        while True:
+            encoded_message = client_socket.recv(1024)
+            
+            # If empty bytes, then the server has disconnected
+            if not encoded_message: break 
+            decoded_message = encoded_message.decode("utf-8").strip()
+            print(decoded_message)
+    except ConnectionResetError:
+        print("\n[Client] Server forcefully disconnected.")
+    except Exception as e:
+        print(f"\n[Client] Error receiving message: {e}")
+
+def start_client(server_ip_address: str, server_port: int) -> None:
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    try:
+        client_socket.connect((server_ip_address, server_port))
+    except Exception as e:
+        print(f"[Client] Unable to connect to server: {e}")
+        return
+    
+    print(f"[Client] Successfully connected to server at {server_ip_address}")
+
+    username_request_bytes = client_socket.recv(1024)
+    username_request = username_request_bytes.decode("utf-8")
+    username = input(username_request)
+    
+    client_socket.sendall(username.encode("utf-8"))
+
+    thread = threading.Thread(target=handle_server, args=(client_socket,))
+    thread.start()
+
+    try:
+        while True:
+            message = input("Enter message: ")
+            encoded_message = message.encode("utf-8")
+            client_socket.sendall(encoded_message)
+    except KeyboardInterrupt:
+        print(f"[Client] Shutting down.")
+    finally:
+        client_socket.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Client initialization for unicast messaging")
+    parser.add_argument("ip", type=str, help="The IP Address of the SERVER you are trying to connect to.")
+    parser.add_argument("port", type=int, help="The port number that the SERVER will listen from")
+
+    args = parser.parse_args()
+    start_client(server_ip_address=args.ip, server_port=args.port)
